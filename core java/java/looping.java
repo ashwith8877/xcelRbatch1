567595e2a84552a35225337ff5350f89ca0ab81e
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-01-03 12:04:08 +0530

    Add files via upload

---
+++ core java/java/looping.java
@@ -0,0 +1,16 @@
+import java.util scanner;
+
+
+class looping{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1,num2;
+        if(num1>num2){
+            System.out.println("num1 is greater");
+
+        }
+        else{
+             System.out.println("num2 is greater");
+        }
+    }
+}
