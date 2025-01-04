1ba26a2200a04bc39727f586a368d1bcd971dc52
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-01-04 18:25:07 +0530

    Add files via upload

---
+++ core java/scannign.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class scanning{
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.println(name);
+      String name = sc.nextLine();
+      System.out.println(age);
+      int age = sc.nextInt();
+       System.out.println(number);
+      int number = sc.nextInt();
+        
+        sc.close();
+        }
+
+    }
+
