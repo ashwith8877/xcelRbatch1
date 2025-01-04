c42944c2273eeac8913a0b598f63b8f7e2f68eca
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-01-04 12:19:23 +0530

    Add files via upload

---
+++ core java/while2.java
@@ -0,0 +1,11 @@
+class elseif{
+    public static void main(String[] args){
+        String name = "ashwith";
+        String pass = "ashwith";
+        while((name == "ashwith") && (pass == "ashwith")){
+             System.out.println("user and password is valid");
+             break;
+        }
+
+    }
+}
