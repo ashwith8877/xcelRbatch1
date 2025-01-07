c1f9444b3e8e48e4890263e78becf93f700abf8e
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-01-07 13:16:39 +0530

    Add files via upload

---
+++ core java/swapping.java
@@ -0,0 +1,12 @@
+class swapping{
+    public static void main(String []args){
+        int a=20;
+        int b=30;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("temp value: "+temp);
+        System.out.println("a value: "+a);
+        System.out.println("b value: "+b);
+    }
+}
