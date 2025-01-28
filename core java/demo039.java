9b948b22b349b8a8b4574adad15349fc5db24268
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-01-28 10:51:36 +0530

    character lowercase or uppercase or digit

---
+++ core java/demo039.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class demo039{
+    public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the character");
+    char ch = sc.next().charAt(0);
+        if(Character.isLowerCase(ch)){
+            System.out.println("character is lower case");
+        }
+        else if(Character.isUpperCase(ch)){
+            System.out.println("character is upper case");
+        }
+        else if(Character.isDigit(ch)){
+            System.out.println("character is Digit");
+
+        }
+        else{
+            System.out.println("character is Special character");
+        }
+}
+}
