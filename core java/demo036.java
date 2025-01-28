d6daaa678dbdcdf6ae2d86cde7912887bd03bdb4
ashwith8877 <ashwithgoudchinnamraju@gmail.com>
2025-01-28 10:49:36 +0530

    Big Number b/w 3 Numbers

---
+++ core java/demo036.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class demo036{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value: ");
+        int a = sc.nextInt();
+        System.out.println("Enter b value: ");
+        int b = sc.nextInt();
+        System.out.println("Enter c value: ");
+        int c = sc.nextInt();
+        if(a>b && a>c){
+            System.out.println( "a is greater than b & c");
+        }
+        else if(b>a && b>c){
+            System.out.println( "b is greater than c & a");
+        }
+        else{
+            System.out.println("c is greater than a & b");
+        }
+        
+    }
+}
